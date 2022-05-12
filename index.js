d36fb669259e57b5d729bc69a236ffa3d77cbc81
Bryan Kwong <bryank458@gmail.com>
2022-05-11 17:57:06 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+var AWS = require("aws-sdk");
+AWS.config.update({ region: "us-west-2" });
+s3 = new AWS.S3({ apiVersion: "2006-03-01" });
+
+s3.listBuckets(function (err, data) {
+  if (err) {
+    console.log("Error", err);
+  } else {
+    console.log("Success", data.Buckets);
+  }
+});
